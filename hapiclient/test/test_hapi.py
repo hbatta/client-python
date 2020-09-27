f9e1fb5ade7d136473a27bdf870cff46051fd7eb
hari <harinarayanabatta1997@gmail.com>
2020-09-26 20:17:11 -0400

    n_splits to n_chunks

--- hapiclient/test/test_hapi.py
+++ hapiclient/test/test_hapi.py
@@ -208,19 +208,19 @@ def test_split():
     data1c, meta1c = hapi(server, dataset, parameters, start, stop, **opts)
     compare(data1, data1c, meta1c, 'Serial, no split, cache')
 
-    opts = {'usecache': False, 'parallel': False, 'n_splits': 5}
+    opts = {'usecache': False, 'parallel': False, 'n_chunks': 5}
     data2, meta2 = hapi(server, dataset, parameters, start, stop, **opts)
     compare(data1, data2, meta2, '....')
 
-    opts = {'usecache': True, 'parallel': False, 'n_splits': 5}
+    opts = {'usecache': True, 'parallel': False, 'n_chunks': 5}
     data2c, meta2c = hapi(server, dataset, parameters, start, stop, **opts)
     compare(data1, data2c, meta2c, '....')
 
-    opts = {'usecache': False, 'parallel': True, 'n_splits': 5}
+    opts = {'usecache': False, 'parallel': True, 'n_chunks': 5}
     data3, meta3 = hapi(server, dataset, parameters, start, stop, **opts)
     compare(data1, data3, meta3, '....')
 
-    opts = {'usecache': True, 'parallel': True, 'n_splits': 5}
+    opts = {'usecache': True, 'parallel': True, 'n_chunks': 5}
     data3c, meta3c = hapi(server, dataset, parameters, start, stop, **opts)
     compare(data1, data3c, meta3c, '....')
 
