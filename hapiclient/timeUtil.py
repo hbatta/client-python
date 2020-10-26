49cbf78743ab5b701abf9d03a409d4d52b29d496
jbfaden <faden@cottagesystems.com>
2020-10-26 13:24:13 -0500

    start to translate code from TimeUtil.java to python version.

---
+++ hapiclient/timeUtil.py
@@ -0,0 +1,319 @@
+
+# 
+# the number of days in each month.
+# 
+DAYS_IN_MONTH = [ [ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0 ],
+    [ 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0 ] ]
+
+
+# 
+# the number of days to the first of each month.
+# 
+DAY_OFFSET = [ [ 0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 ],
+    [ 0, 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 ] ]
+
+def is_leap_year( year ):
+    if year<1582 or year>2400:
+        raise Exception("year must be between 1582 and 2400")
+    return (year % 4) == 0 and (year % 400 == 0 or year % 100 != 0)  
+        
+# 
+# return the doy of year of the month and day for the year. For example, in
+# @param year the year
+# @param month the month, from 1 to 12.
+# @param day the day in the month.
+# @return the day of year.
+# 
+def day_of_year( year, month, day) :
+    if (month == 1) :
+        return day
+    
+    if (month < 1) :
+        raise Exception("month must be greater than 0.")
+    
+    if (month > 12) :
+        raise Exception("month must be less than 12.")
+    
+    if (day > 366) :
+        raise Exception("day (" + day + ") must be less than 366.")
+    
+    if is_leap_year(year):
+        return DAY_OFFSET[1][month] + day
+    else:
+        return DAY_OFFSET[0][month] + day
+
+
+# 
+# normalize the decomposed time by expressing day of year and month and day
+# of month, and moving hour="24" into the next day. This also handles day
+# increment or decrements, by:<ul>
+# <li>handle day=0 by decrementing month and adding the days in the new
+# month.
+# <li>handle day=32 by incrementing month.
+# <li>handle negative components by borrowing from the next significant.
+# </ul>
+# Note that [Y,1,dayOfYear,...] is accepted, but the result will be Y,m,d.
+# @param time the seven-component time Y,m,d,H,M,S,nanoseconds
+# 
+def normalize_time(time) :
+    #logger.entering( "TimeUtil", "normalizeTime" )
+    while (time[3] >= 24) :
+        time[2] += 1
+        time[3] -= 24
+    
+    if (time[6] < 0) :
+        time[5] -= 1
+        time[6] += 1000000000
+    
+    if (time[5] < 0) :
+        time[4] -= 1; # take a minute
+        time[5] += 60; # add 60 seconds.
+    
+    if (time[4] < 0) :
+        time[3] -= 1; # take an hour
+        time[4] += 60; # add 60 minutes
+    
+    if (time[3] < 0) :
+        time[2] -= 1; # take a day
+        time[3] += 24; # add 24 hours
+    
+    if (time[2] < 1) :
+        time[1] -= 1; # take a month
+        leap= isLeapYear(time[0])
+        if ( time[1]==0 ):
+            extraDays= 31
+        else:
+            extraDays= DAYS_IN_MONTH[ leap ][ time[1] ]
+        daysInMonth =  daysInMonth + extraDays
+        time[2] += daysInMonth; # add 24 hours
+    
+    if (time[1] < 1) :
+        time[0] = time[0] - 1; # take a year
+        time[1] = time[1] + 12; # add 12 months
+    
+    if (time[3] > 24) :
+        raise Exception("time[3] is greater than 24 (hours)")
+    
+    if (time[1] > 12) :
+        time[0]= time[0] + 1
+        time[1]= time[1] - 12
+    
+    if (time[1] == 12 and time[2]>31 and time[2]<62 ) :
+        time[0] = time[0] + 1
+        time[1] = 1
+        time[2] = time[2]-31
+        logger.exiting( "TimeUtil", "normalizeTime" )
+        return
+    
+    if ( is_leap_year(time[0]) ):
+        leap= 1
+    else:
+        leap= 0
+    
+    if (time[2] == 0) :
+        time[1] = time[1] - 1
+        if (time[1] == 0) :
+            time[0] = time[0] - 1
+            time[1] = 12
+        
+        time[2] = DAYS_IN_MONTH[leap][time[1]]
+    
+    d = DAYS_IN_MONTH[leap][time[1]]
+    while (time[2] > d) :
+        time[1] = time[1] + 1
+        time[2] -= d
+        d = DAYS_IN_MONTH[leap][time[1]]
+        if (time[1] > 12) :
+            raise Exception("time[2] is too big")
+    
+    #logger.exiting( "TimeUtil", "normalizeTime" )
+
+
+
+# 
+# return $Y-$m-$dT$H:$M:$S.$(subsec,places=9)Z
+# 
+# @param time any isoTime format string.
+# @return the time in standard form.
+# 
+def normalize_time_string(time) :
+    nn = iso_time_to_array(time)
+    normalize_time(nn)
+    return "%d-%02d-%02dT%02d:%02d:%02d.%09dZ" % ( nn[0], nn[1], nn[2], nn[3], nn[4], nn[5], nn[6] )
+
+
+# 
+# Rewrite the time using the format of the example time. For example,
+# <pre>
+# :@code
+    # from org.hapiserver.TimeUtil import *
+    # print rewriteIsoTime( '2020-01-01T00:00Z', '2020-112Z' ) # ->  '2020-04-21T00:00Z'
+    # 
+# </pre>
+# This allows direct comparisons of times for sorting.
+# TODO: there's an optimization here, where if input and output are both $Y-$j or
+# both $Y-$m-$d, then we need not break apart and recombine the time
+# (isoTimeToArray call can be avoided).
+# 
+# @param exampleForm isoTime string.
+# @param time the time in any allowed isoTime format
+# @return same time but in the same form as exampleForm.
+# 
+def reformat_iso_time( exampleForm, time):
+    c = exampleForm[8]
+    nn = iso_time_to_array( normalize_time_string(time) )
+    if c=='T':
+        # $Y-$jT
+        nn[2] = day_of_year(nn[0], nn[1], nn[2])
+        nn[1] = 1
+        time = "%d-%03dT%02d:%02d:%02d.%09dZ" % ( nn[0], nn[2], nn[3], nn[4], nn[5], nn[6] )
+    elif c=='Z':
+        nn[2] = day_of_year(nn[0], nn[1], nn[2])
+        nn[1] = 1
+        time = "%d-%03dZ" % ( nn[0], nn[2] )
+    else:
+        if ( len(exampleForm) == 10) :
+            c = 'Z'
+        else :
+            c = exampleForm[10]
+        
+        if (c == 'T') :
+            # $Y-$jT
+            time = "%d-%02d-%02dT%02d:%02d:%02d.%09dZ" % ( nn[0], nn[1], nn[2], nn[3], nn[4], nn[5], nn[6])
+        elif (c == 'Z') :
+            time = "%d-%02d-%02dZ" % ( nn[0], nn[1], nn[2])
+            
+    if (exampleForm.endswith("Z")) :
+        return time[ 0:len(exampleForm)- 1 ] + "Z"
+    else :
+        return time[ 0:len(exampleForm) ]
+    
+
+# return array [ year, months, days, hours, minutes, seconds, nanoseconds ]
+# preserving the day of year notation if this was used. See the class
+# documentation for allowed time formats, which are a subset of ISO8601
+# times.  This also supports "now", "now-P1D", and other simple extensions.
+# The following are valid inputs:<ul>
+# <li>2021
+# <li>2020-01-01
+# <li>2020-01-01Z
+# <li>2020-01-01T00Z
+# <li>2020-01-01T00:00Z
+# <li>now
+# <li>now-P1D
+# <li>lastday-P1D
+# <li>lasthour-PT1H
+# </ul>
+# 
+# @param time isoTime to decompose
+# @return the decomposed time
+# @throws IllegalArgumentException when the time cannot be parsed.
+# @see #isoTimeFromArray(int[])
+# 
+def iso_time_to_array( time ) :
+    time= time.strip()
+    if (len(time) == 4) :
+        result = [ int(time), 1, 1, 0, 0, 0, 0 ]
+    elif ( time.startswith("now") or time.startswith("last") ) :
+        remainder= None
+        if ( time.startswith("now") ) :
+            n= now()
+            remainder= time.substring(3)
+        else :
+            p= Pattern.compile("last([a-z]+)([\\+|\\-]P.*)?")
+            m= p.matcher(time)
+            if m.matches() :
+                n= now()
+                unit= m.group(1)
+                remainder= m.group(2)
+                if unit=="year":
+                    idigit= 1
+                elif unit=="month":
+                    idigit= 2
+                elif unit=="day":
+                    idigit= 3
+                elif unit=="hour":
+                    idigit= 4
+                elif unit=="minute":
+                    idigit= 5
+                elif unit=="second":
+                    idigit= 6
+                else:
+                    raise exception("unsupported unit: "+unit)
+                
+                if idigit>1: istart=idigit 
+                else: istart=1
+                for id in xrange( istart, 3 ):
+                    n[id]= 1
+                
+                if idigit>3: istart=idigit
+                else: istart=3
+                for id in xrange( istart, 7 ):
+                    n[id]= 0
+                
+            else :
+                raise exception("expected lastday+P1D, etc")
+            
+        
+        if remainder==None or remainder.length()==0 :
+            return n
+        elif remainder.charAt(0)=='-' :
+            return subtract( n, parseISO8601Duration(remainder.substring(1)) )
+            
+        elif remainder.charAt(0)=='+' :
+            return add( n, parseISO8601Duration(remainder.substring(1)) )
+        
+        return now()
+        
+    else :
+        if len(time) < 7 :
+            raise Exception("time must have 4 or greater than 7 elements")
+        
+        # first, parse YMD part, and leave remaining components in time.
+        if len(time)==7 :
+            result = [ int(time.substring[0:4]), int(time[5:7]), 1, # days
+                0, 0, 0, 0 ]
+            time = ""
+        elif len(time) == 8 :
+            result = [ int(time[0:4]), 1, int(time[5,8]), # days
+                0, 0, 0, 0 ]
+            time = ""
+        elif time[8] == 'T':
+            result = [ int(time[0:4]), 1, int(time[5:8]), # days
+                0, 0, 0, 0 ]
+            time = time[9:]
+        elif (time[8] == 'Z') :
+            result =  [ int(time[0:4]), 1, int(time[5:8]), # days
+                0, 0, 0, 0 ]
+            time = time[9:]
+        else :
+            result = [ int(time[0:4]), int(time[5:7]), int(time[8:10]), 0, 0, 0, 0 ]
+            if len(time) == 10 :
+                time = ""
+            else :
+                time = time[11:]
+            
+        
+        # second, parse HMS part.
+        if time.endswith("Z") :
+            time = time[0:-1]
+        
+        if len(time) >= 2 :
+            result[3] = int(time[0:2])
+        
+        if len(time) >= 5 :
+            result[4] = int(time[3:5])
+        
+        if len(time) >= 8 :
+            result[5] = int(time[6:8])
+        
+        if len(time) > 9 :
+            import math
+            result[6] = int( (math.pow(10, 18 - len(time))) * int(time[9:]) )
+        
+        normalize_time(result)
+    
+    return result
+
+#print iso_time_to_array( '2030-04-05T00:11Z' )
+#print reformat_iso_time('2030-003T00:00Z','2030-04-05T00:11Z')
